f48db5eae52625273da301f09f02de5c004bc6af
Alexander Wojtkowski <alexander.wojtkowski@torontomu.ca>
2025-10-28 15:08:27 -0400

    Added Data and Training Algo
    Setup such that if a model wants to be trained or loaded can be selected

--- Project 2/Project 2 Training.py
+++ Project 2/Project 2 Training.py
@@ -4,39 +4,120 @@
 
 # Due Date: November 5th, 2025
 
-""" Imports """
+""" IMPORTS """
 
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+import tensorflow as tf
 from tensorflow import keras
-from tensorflow.keras import layers
+from tensorflow.keras.models import load_model
 
-warnings.filterwarnings("ignore", category=ConvergenceWarning)
+""" VARIABLES """
+
+np.random.seed(42)
+keras.utils.set_random_seed(42)
+
+TRAIN_MODEL = 0 # 1 to train model, 0 to load model
 
 """ Part 1: Data Processing - 20 marks """
 # Define Image Shape (500,500,3)
+IMG_HEIGHT = 500
+IMG_WIDTH = 500
+IMG_Shape = (IMG_HEIGHT,IMG_WIDTH,3) # Height Width Channels
 
 # Establish Train and Validation Data Directories
+training_dir = "Data/train"
+validation_dir = "Data/valid"
 
 # Perform Data Augmentation (Re-scaling, shear range, zoom range)
 # Use Keras' Image Preprocessing Pipeline or Torchvision Transforms
 
+normalization_layer = keras.layers.Rescaling(1/255)
+
+data_augmentation = keras.Sequential([
+    keras.layers.RandomFlip("horizontal"),
+    keras.layers.RandomRotation(0.1),
+    keras.layers.RandomZoom(0.1),
+])
+
 # Create Train and Validation Generator Using Keras' imagedatasetfromdirectory
 # or PyTorch's Dataloader
+train_ds = keras.utils.image_dataset_from_directory(
+    training_dir,
+    image_size=(IMG_HEIGHT, IMG_WIDTH),
+    batch_size=32,
+    shuffle=True
+)
 
-""" Part 2: Neural Network Architecture Design - 30 marks """
-# We now build custom neural network
+valid_ds = keras.utils.image_dataset_from_directory(
+    validation_dir,
+    image_size=(IMG_HEIGHT, IMG_WIDTH),
+    batch_size=32,
+    shuffle=False
+)
 
-# Convolutional Layers (Conv2D) Explore: # of filters, kernal size, stride
+train_ds = train_ds.map(lambda x, y: (data_augmentation(normalization_layer(x)), y))
+valid_ds = valid_ds.map(lambda x, y: (normalization_layer(x), y))
 
-# MaxPooling2D Layer, pool extracted features
+AUTOTUNE = tf.data.AUTOTUNE
 
-# Flatten Layer
+# Optimize data pipeline performance
+train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
+valid_ds = valid_ds.cache().prefetch(buffer_size=AUTOTUNE)
+    
 
-# Dense and Dropout Layers, to perform final predictions
-# Final dense should only have 3 neurons, correlating to label classes
+if TRAIN_MODEL == 1:
+    """ Part 2: Neural Network Architecture Design - 30 marks """
+    # We now build custom neural network
+    model = keras.Sequential([
+        keras.layers.Input(shape=IMG_Shape),
+        
+    # Convolutional Layers (Conv2D) Explore: # of filters, kernal size, stride
+    # MaxPooling2D Layer, pool extracted features
+        keras.layers.Conv2D(32, (3,3), activation='relu', padding='same'),
+        keras.layers.MaxPooling2D(pool_size=(2,2)),
+        
+        keras.layers.Conv2D(64, (3,3), activation='relu', padding='same'),
+        keras.layers.MaxPooling2D(pool_size=(2,2)),
+        
+        keras.layers.Conv2D(128, (3,3), activation='relu', padding='same'),
+        keras.layers.MaxPooling2D(pool_size=(2,2)),
+    
+    # Flatten Layer
+        keras.layers.Flatten(),
+        
+    # Dense and Dropout Layers, to perform final predictions
+    # Final dense should only have 3 neurons, correlating to label classes
+        keras.layers.Dense(128, activation='relu'),
+        keras.layers.Dropout(0.5),
+    
+        keras.layers.Dense(3, activation='softmax')
+        ])
+    
+    model.summary()
+    
+    model.compile(
+        optimizer='adam',
+        loss='sparse_categorical_crossentropy',  # because labels are integer-encoded by image_dataset_from_directory
+        metrics=['accuracy']
+    )
+    
+    history = model.fit(
+        train_ds,
+        validation_data=valid_ds,
+        epochs=10
+    )
+    
+    model.save('First_Model.keras')
+    
+    loss, acc = model.evaluate(valid_ds)
+    print(f"Validation Accuracy: {acc:.2f}")
+else:
+    loaded_model = load_model('First_Model.keras')
+    loss, acc = loaded_model.evaluate(valid_ds)
+    print(f"Validation Accuracy: {acc:.2f}")
 
 """ Part 3: Hyperparameter Analysis - 20 marks """
 # Common Functions within Convolution layers are relu and LeakyRelu
